e97b009e674b37593f2b8721561ee536de2764b5
Mohamed Amine Nadim <mohamedamine.nadim@rossel.fr>
2024-06-18 21:17:40 +0200

    feature/Initialized module + GCP BD connection

--- produits/src/main/java/com/epsi/mspr/payetonkawa/produits/ProduitEntity.java
+++ produits/src/main/java/com/epsi/mspr/payetonkawa/produits/ProduitEntity.java
@@ -3,7 +3,7 @@ package com.epsi.mspr.payetonkawa.produits;
 import lombok.Data;
 
 @Data
-public class Produit {
+public class ProduitEntity {
     private static final long uid = 0L;
     private String name;
     private double unitaryPrice;
