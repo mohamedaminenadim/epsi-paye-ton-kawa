a28e77bf87e51ecec420c2986b89024feafcd9cc
Mohamed Amine Nadim <mohamedamine.nadim@rossel.fr>
2024-06-18 21:16:36 +0200

    feature/Added service and controller to Commande API + GCP DB connection

--- commandes/src/main/java/com/epsi/mspr/payetonkawa/commandes/CommandeEntity.java
+++ commandes/src/main/java/com/epsi/mspr/payetonkawa/commandes/CommandeEntity.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-public class Commande {
+public class CommandeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long commandeID;
---
+++ commandes/src/main/java/com/epsi/mspr/payetonkawa/commandes/CommandesService.java
@@ -0,0 +1,32 @@
+package com.epsi.mspr.payetonkawa.commandes;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CommandesService {
+
+    private CommandesRepository commandesRepository;
+
+    public List<CommandeEntity> getAllCommandes() {
+        return this.commandesRepository.findAll();
+    }
+    public CommandeEntity getCommandeById(Long id) {
+        return this.commandesRepository.findByCommandeID(id);
+    }
+
+    public CommandeEntity postCommande(CommandeEntity commandeEntity) {
+        return this.commandesRepository.save(commandeEntity);
+    }
+
+    public CommandeEntity updateCommande(Long id, CommandeEntity commandeEntity) {
+        CommandeEntity toUpdate = this.commandesRepository.findByCommandeID(id);
+        // Set all fields
+        return toUpdate;
+    }
+
+    public CommandeEntity deleteClient(Long id) {
+        return this.commandesRepository.deleteCommandeEntityByCommandeID(id);
+    }
+}
